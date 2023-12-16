3ad0f2a0d441a4bf689635bf20bc24c6c1c8cf5d
ajarwee <ajarwee@byui.edu>
2023-12-15 19:08:33 -0700

    changes

--- FinalProject/Finalproject.cs
+++ FinalProject/Finalproject.cs
@@ -1,108 +1,124 @@
 using System;
 using System.Collections.Generic;
 
-// Book Class
-public class Book
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public bool IsAvailable { get; set; }
-
-    public void CheckOut()
-    {
-        IsAvailable = false;
-    }
-
-    public void CheckIn()
-    {
-        IsAvailable = true;
-    }
-}
-
-// LibraryMember Class
-public class LibraryMember
-{
-    public int MemberID { get; set; }
-    public string Name { get; set; }
-    public List<Transaction> BorrowingHistory { get; set; }
-
-    public LibraryMember()
-    {
-        BorrowingHistory = new List<Transaction>();
-    }
-
-    public void BorrowBook(Book book)
-    {
-        Transaction transaction = new Transaction(this, book);
-        BorrowingHistory.Add(transaction);
-    }
-
-    public void ReturnBook(Transaction transaction)
-    {
-        transaction.CompleteTransaction();
-    }
-}
-
-// Transaction Class
-public class Transaction
-{
-    public Book Book { get; set; }
-    public LibraryMember Member { get; set; }
-    public DateTime IssueDate { get; set; }
-    public DateTime DueDate { get; set; }
-    public bool IsReturned { get; set; }
-    public double Fine { get; set; }
-
-    public Transaction(LibraryMember member, Book book)
-    {
-        Member = member;
-        Book = book;
-        IssueDate = DateTime.Now;
-        DueDate = IssueDate.AddDays(14); // Assuming a 14-day borrowing period
-        IsReturned = false;
-        Fine = 0;
-    }
-
-    public void CalculateFine()
-    {
-        if (DateTime.Now > DueDate && !IsReturned)
-        {
-            // Calculate fine logic (you can customize this based on your requirements)
-            TimeSpan overdue = DateTime.Now - DueDate;
-            Fine = overdue.Days * 0.5; // Assuming a fixed fine of $0.50 per day
-        }
-    }
-
-    public void CompleteTransaction()
-    {
-        if (!IsReturned)
-        {
-            CalculateFine();
-            Book.CheckIn();
-            IsReturned = true;
-        }
-    }
-}
-
-// Example Program Execution Flow
 class Program
 {
     static void Main()
     {
-        // Initialization
-        Book book1 = new Book { Title = "Book1", Author = "Author1", IsAvailable = true };
-        Book book2 = new Book { Title = "Book2", Author = "Author2", IsAvailable = true };
+        // Example usage of the Library Management System
+        Library library = new Library();
 
-        LibraryMember member1 = new LibraryMember { MemberID = 1, Name = "Member1" };
+        Book book1 = new Book("The Catcher in the Rye", "J.D. Salinger", "12345");
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "67890");
 
-        // Transaction Initiation
-        member1.BorrowBook(book1);
+        library.AddBook(book1);
+        library.AddBook(book2);
 
-        // Fine Calculation
-        Transaction transaction = member1.BorrowingHistory[0];
-        transaction.CalculateFine();
+        Member member1 = new Member("M001", "John Doe");
+        Member member2 = new Member("M002", "Jane Smith");
 
-        // Transaction Completion
-        member1.ReturnBook(transaction);
+        library.AddMember(member1);
+        library.AddMember(member2);
+
+        library.DisplayBooks();
+        library.DisplayMembers();
+
+        // Simulate a book transaction
+        library.BorrowBook(book1, member1);
+        library.DisplayBooks();
+        library.DisplayMembers();
+
+        library.ReturnBook(book1);
+        library.DisplayBooks();
+        library.DisplayMembers();
+    }
+}
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    public Book(string title, string author, string isbn)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+    }
+}
+
+class Member
+{
+    public string MemberID { get; set; }
+    public string Name { get; set; }
+    public List<Book> BorrowedBooks { get; set; } = new List<Book>();
+
+    public Member(string memberID, string name)
+    {
+        MemberID = memberID;
+        Name = name;
+    }
+}
+
+class Library
+{
+    private List<Book> books = new List<Book>();
+    private List<Member> members = new List<Member>();
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    public void AddMember(Member member)
+    {
+        members.Add(member);
+    }
+
+    public void BorrowBook(Book book, Member member)
+    {
+        if (book.IsAvailable)
+        {
+            book.IsAvailable = false;
+            member.BorrowedBooks.Add(book);
+            Console.WriteLine($"{member.Name} borrowed {book.Title}.");
+        }
+        else
+        {
+            Console.WriteLine($"{book.Title} is not available for borrowing.");
+        }
+    }
+
+    public void ReturnBook(Book book)
+    {
+        if (!book.IsAvailable)
+        {
+            book.IsAvailable = true;
+            Console.WriteLine($"Book {book.Title} returned.");
+        }
+        else
+        {
+            Console.WriteLine($"Error: Book {book.Title} is already available.");
+        }
+    }
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine("\nLibrary Books:");
+        foreach (var book in books)
+        {
+            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, ISBN: {book.ISBN}, Available: {book.IsAvailable}");
+        }
+    }
+
+    public void DisplayMembers()
+    {
+        Console.WriteLine("\nLibrary Members:");
+        foreach (var member in members)
+        {
+            Console.WriteLine($"MemberID: {member.MemberID}, Name: {member.Name}, Borrowed Books: {member.BorrowedBooks.Count}");
+        }
     }
 }
